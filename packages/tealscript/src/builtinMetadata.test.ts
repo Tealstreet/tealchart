9c81de563264cf3b7b713568ed31c40fa3e530e0
Sam <sam@tealstreet.io>
2026-06-02 09:45:46 +0800

    Assert exportable metadata coverage is nonempty

--- packages/tealscript/src/builtinMetadata.test.ts
+++ packages/tealscript/src/builtinMetadata.test.ts
@@ -6,6 +6,7 @@ import {
 describe('builtin metadata', () => {
   it('recognizes exportable constant roots as builtin namespaces', () => {
     const roots = new Set([...EXPORTABLE_BUILTIN_CONSTANTS].map((name) => name.split('.')[0]));
+    expect(roots.size).toBeGreaterThan(0);
 
     for (const root of roots) {
       expect(BUILTIN_NAMESPACES.has(root)).toBe(true);
