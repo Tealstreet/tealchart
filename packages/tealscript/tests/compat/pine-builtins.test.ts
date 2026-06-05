cca7d38a77b664cf8ccb35caf97aa2005725c445
Sam <sam@tealstreet.io>
2026-06-05 19:27:36 +0800

    Support Pine format_time week of year

--- packages/tealscript/tests/compat/pine-builtins.test.ts
+++ packages/tealscript/tests/compat/pine-builtins.test.ts
@@ -559,6 +559,7 @@ weekdayNameTime = str.format_time(timestamp("UTC", 2024, 8, 20), "E EEEE", "UTC"
 dayOfYearTime = str.format_time(timestamp("UTC", 2024, 1, 5), "D DD DDD", "UTC")
 singleYearTime = str.format_time(timestamp("UTC", 2024, 8, 20), "MMM-d-y", "UTC")
 timezoneNameTime = str.format_time(timestamp("UTC", 2024, 1, 5), "z zzzz", "UTC")
+weekOfYearTime = str.format_time(timestamp("UTC", 2024, 1, 5), "w ww", "UTC")
 plot(formatted == "102.00", title="Formatted Close")
 plot(prefixFormatted == "102.00", title="Prefix Formatted Close")
 plot(message == "close=102.0", title="Format Template")
@@ -578,6 +579,7 @@ plot(weekdayNameTime == "Tue Tuesday", title="Weekday Name Formatted Time")
 plot(dayOfYearTime == "5 05 005", title="Day Of Year Formatted Time")
 plot(singleYearTime == "Aug-20-2024", title="Single Year Formatted Time")
 plot(timezoneNameTime == "UTC Coordinated Universal Time", title="Timezone Name Formatted Time")
+plot(weekOfYearTime == "1 01", title="Week Of Year Formatted Time")
 `);
 
     expect(result.errors).toEqual([]);
