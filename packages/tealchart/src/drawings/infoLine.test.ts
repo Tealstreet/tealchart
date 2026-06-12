05fa7a781c7c401a0d211d79266c806f85a1d367
Sam <sam@tealstreet.io>
2026-06-12 08:51:21 +0800

    Add bar counts to forecast labels

--- packages/tealchart/src/drawings/infoLine.test.ts
+++ packages/tealchart/src/drawings/infoLine.test.ts
@@ -15,7 +15,25 @@ describe('user drawing info line metrics', () => {
       priceDelta: 25,
       percent: 25,
       deltaMs: 60_000,
+      barCount: null,
       label: '+25.00 (+25.00%) / 1 minute',
+    });
+  });
+
+  it('includes inclusive bar counts when bar data is available', () => {
+    expect(
+      resolveUserDrawingInfoLineMetrics({ time: 0, price: 100 }, { time: 60_000, price: 125 }, [
+        { time: 0 },
+        { time: 30_000 },
+        { time: 60_000 },
+        { time: 90_000 },
+      ]),
+    ).toEqual({
+      priceDelta: 25,
+      percent: 25,
+      deltaMs: 60_000,
+      barCount: 3,
+      label: '+25.00 (+25.00%) / 3 bars, 1 minute',
     });
   });
 });
